e7acbd39aec77e1ca8b13716c390e2cdc0b0998d
Li Zheng <lizheng7@360.com>
2022-09-04 11:13:36 +0800

    to complete power

--- NumberOfOneInBainary/Source.cpp
+++ NumberOfOneInBainary/Source.cpp
@@ -1,20 +1,91 @@
 #include<stdio.h>
 
-int NumberOfOneInBinary(int value)
+int NumberOfOneInBinaryByAssembly(int value)
 {
 	int sum = 0;
-	if (value == 0) return 0;
-	if (value < 0) {
-		sum++;
-		value <<= 1;
-		value >>= 1;
+	int andValue;
+	while(value)
+	{
+		andValue = value & 0x1;
+		if (andValue == 1) {
+			sum++;
+			_asm {
+				shr value, 0x1
+			}
+		}
+		else
+		{
+			__asm {
+				shr value, 0x1
+			}
+		}
 	}
-	return 0;
-
+	return sum;
 }
 
-int main()
+int NumberOfOneInBinary(int n)
 {
-	NumberOfOneInBinary(-3);
-	return 0;
+    int count = 0;
+    unsigned int flag = 1;
+    while (flag)
+    {
+        if (n & flag)
+            count++;
+
+        flag = flag << 1;
+    }
+
+    return count;
 }
+
+int perfect(int n)
+{
+    int count = 0;
+    unsigned int flag;
+    while (n)
+    {
+        count++;
+        n = (n - 1) & n; //n - 1 only change rightmost 1 to 0, and left all is changed to 0. 
+    }
+    return count;
+}
+
+// ====================测试代码====================
+void Test(int number, unsigned int expected)
+{
+    int actual = NumberOfOneInBinary(number);
+    if (actual == expected)
+        printf("Test for %d passed.\n", number);
+    else
+        printf("Test for %d failed.\n", number);
+}
+
+int main(int argc, char* argv[])
+{
+    // 输入0，期待的输出是0
+    Test(0, 0);
+
+    // 输入1，期待的输出是1
+    Test(1, 1);
+
+    // 输入10，期待的输出是2
+    Test(10, 2);
+
+    // 输入0x7FFFFFFF，期待的输出是31
+    Test(0x7FFFFFFF, 31);
+
+    // 输入0xFFFFFFFF（负数），期待的输出是32
+    Test(0xFFFFFFFF, 32);
+
+    // 输入0x80000000（负数），期待的输出是1
+    Test(0x80000000, 1);
+
+    return 0;
+}
+
+
+/*
+Think:
+1. about for loop to while loop: make my program flexible
+2.可以一直右移，右移就是除2 
+*/
---
+++ pow/Source.cpp
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+bool equal(double valueOne, double valueTwo)
+{
+    if (valueOne - valueTwo > 0.0000001 || valueTwo - valueOne > 0.0000001) return false;
+    return true;
+}
+
+double GetPowValueWithUnsignedExponent(double base, unsigned int exponent)
+{
+    /*double result = 1.0;
+    for (int i = 0; i < exponent; i++)
+    {
+        result *= base;
+    }
+    return result;*/
+
+    
+}
+
+double GetPowValue(double base, int exponent)
+{
+    if (exponent == 0) return 1;
+    if (equal(base, 0)) return 0;
+
+    unsigned int absExponent = exponent;
+    if (exponent < 0) absExponent = -exponent;
+
+    double result = GetPowValueWithUnsignedExponent(base, absExponent);
+
+    if (exponent < 0) return 1 / result;
+
+    return result;
+}
+
+// ====================测试代码====================
+void Test(const char* testName, double base, int exponent, double expectedResult, bool expectedFlag)
+{
+    printf(testName); printf(":\n");
+    double result = GetPowValue(base, exponent);
+    printf("the result: %f\n", result);
+    printf("the expect result: %f\n", expectedResult);
+}
+
+int main(int argc, char* argv[])
+{
+    // 底数、指数都为正数
+    Test("Test1", 2, 3, 8, false);
+
+    // 底数为负数、指数为正数
+    Test("Test2", -2, 3, -8, false);
+
+    // 指数为负数
+    Test("Test3", 2, -3, 0.125, false);
+
+    // 指数为0
+    Test("Test4", 2, 0, 1, false);
+
+    // 底数、指数都为0
+    Test("Test5", 0, 0, 1, false);
+
+    // 底数为0、指数为正数
+    Test("Test6", 0, 4, 0, false);
+
+    // 底数为0、指数为负数
+    Test("Test7", 0, -4, 0, true);
+
+    return 0;
+}
