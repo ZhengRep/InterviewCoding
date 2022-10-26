4dae0d5506bbadf5e7255011d7e2e7a2fd3b13ce
ZhengLi <2726401288@qq.com>
2022-10-26 17:24:35 +0800

    add OneAppearNumFromOneToN_logn

--- 1ShowNumFrom1ToN/Source.cpp
+++ 1ShowNumFrom1ToN/Source.cpp
@@ -1,4 +1,8 @@
-#include<cstdio>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#pragma warning(disable: 4996)
 
 int OneSumInNum(int num)
 {
@@ -28,10 +32,58 @@ int OneAppearNumFromOneToN(int N)
 	return number;
 }
 
+int PowerBase10(int n)
+{
+	int result = 1;
+	for (int i = 0; i < n; i++)
+	{
+		result *= 10;
+	}
+	return result;
+}
+
+int NumberOf1(char* str)
+{
+	if (str == nullptr || *str < '0' || *str > '9') return 0;
+
+	int firstNum = *str - '0';
+	int lenght = strlen(str);
+
+	if (firstNum == 0 && lenght == 1) return 0;
+	if (firstNum > 0 && lenght == 1) return 1;
+
+	//e.g. 21345
+	int numFirstDigit = 0;
+	if (firstNum == 1) {
+		numFirstDigit = atoi(str + 1) + 1;
+	}
+	else if (firstNum > 1) {
+		numFirstDigit = PowerBase10(lenght - 1);
+	}
+
+	//except first num : 01345 - 21345
+	int numOtherDigit = firstNum * (lenght - 1) * PowerBase10(lenght - 2); // one of 1 other permutation
+	//1 - 1345
+	int numRecursive = NumberOf1(str + 1);
+
+	return numFirstDigit + numOtherDigit + numRecursive;
+
+}
+
+int OneAppearNumFromOneToN_logn(int N)
+{
+	if (N < 1) return 0;
+
+	char str[50];
+	sprintf(str, "%d", N);
+
+	return NumberOf1(str);
+}
+
 void Test(const char* testName, int N, int expectValue)
 {
 	printf("%s\n", testName);
-	if (OneAppearNumFromOneToN(N) == expectValue) {
+	if (OneAppearNumFromOneToN_logn(N) == expectValue) {
 		printf("Passed!\n");
 	}
 	else {
